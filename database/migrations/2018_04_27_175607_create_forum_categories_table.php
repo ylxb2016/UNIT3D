14daa921bf4c13b335c1c62d4f950481c7b3e302
poppabear8883 <servnx@gmail.com>
2018-04-27 16:27:38 -0400

    (WIP) Forums Re-Write
    This is a "Work In Progress" commit. Its not expected to work

--- database/migrations/2018_04_27_175607_create_forum_categories_table.php
+++ database/migrations/2018_04_27_175607_create_forum_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateForumChildrenTable extends Migration
+class CreateForumCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
