a492c5d3b50586c949824a3d62ac97db73d1cb07
HDVinnie <unit3d@protonmail.com>
2018-05-23 23:04:49 -0400

    (Update) Add Migration

---
+++ database/migrations/2018_05_24_024732_create_recipes_table.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('category_id')->index('fk_table1_categories1_idx');
+            $table->string('type')->index('type');
+            $table->string('imdb')->default(0)->index('imdb');
+            $table->string('tmdb')->default(0)->index('tmdb');
+            $table->string('status');
+            $table->string('info_hash')->nullable()->index('info_hash');
+            $table->integer('user_id')->index('fk_torrents_users1_idx');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
