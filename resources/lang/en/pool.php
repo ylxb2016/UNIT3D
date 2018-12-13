6822d39a1f4c4bfaaa9780d3b087882ed6fdbe95
HDVinnie <unit3d@protonmail.com>
2018-12-13 15:30:45 -0500

    (Add) Language :new:

---
+++ resources/lang/en/pool.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | Pool Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    'add-bon'      => 'Contribute BON To Pool!',
+    'contribute'   => 'Contribute',
+    'contribution' => 'Contribution',
+    'enter-bp'     => 'Enter bonus points (minimum 1)',
+    'not-active'   => 'The BON Pool is currently not activated. The previous pools reward is still active. Check back for the next pool once Global Freeleech ends.',
+    'pool'         => 'Pool',
+];
